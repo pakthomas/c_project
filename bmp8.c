bd14ca1caa9ce3841e132fbc6ab6a558979f8909
Thomas <pakthomas15@gmail.com>
2025-03-28 10:30:28 +0100

    added header file and .c file

--- bmp8.c
+++ bmp8.c
@@ -1,7 +1,22 @@
 //
 // Created by Thomas on 3/21/2025.
 //
-t_bmp8 * bmp8_loadImage(const char * filename);
-void bmp8_saveImage(const char * filename, t_bmp8 * img);
-void bmp8_free(t_bmp8 * img);
-void bmp8_printInfo(t_bmp8 * img);
+#include <stdio.h>
+#include "bmp8.h"
+t_bmp8 * bmp8_loadImage(const char * filename) {
+    FILE* fptr;
+    fptr = fopen(filename, "r");
+
+    if (fptr == NULL) {
+        printf("The file is not opened. The program will "
+               "now exit.");
+        return 0;
+    }
+    unsigned char header[54];
+    // We know that the image width is stored at offset 18 of the header
+    unsigned int width = *(unsigned int *)&header[18];
+    unsigned int width = *(unsigned int *)(header + 18);
+
+
+    return 0;
+}
