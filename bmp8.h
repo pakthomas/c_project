c0c14a30c45024c36ec59435bd8064e374f10ec5
laure <laure.szymanski@efrei.net>
2025-04-27 01:04:00 +0200

    def

--- bmp8.h
+++ bmp8.h
@@ -25,3 +25,13 @@ t_bmp8 * bmp8_loadImage(const char * filename);
 void bmp8_saveImage(const char * filename, t_bmp8 * img);
 void bmp8_free(t_bmp8 * img);
 void bmp8_printInfo(t_bmp8 * img);
+
+//definition of the treatment function
+void bmp8_negative(t_bmp8 *img);
+void bmp8_brightness(t_bmp8 *img, int value);
+void bmp8_threshold(t_bmp8 *img, int threshold);
+
+//definition of the apllyfilter function
+void bmp8_applyFilter(t_bmp8 *img, float **kernel, int kernelSize);
+
+#endif
