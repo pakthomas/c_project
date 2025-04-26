9a2318d0ad0fec10d6671aa71e843c5fc1951a9f
laure <laure.szymanski@efrei.net>
2025-04-27 01:04:07 +0200

    comments

--- bmp8.h
+++ bmp8.h
@@ -20,7 +20,8 @@ void bmp8_free(t_bmp8 * img);
 void bmp8_printInfo(t_bmp8 * img);
 #endif //BMP8_H
 
-//definition of the function
+//definition of the functions
+
 t_bmp8 * bmp8_loadImage(const char * filename);
 void bmp8_saveImage(const char * filename, t_bmp8 * img);
 void bmp8_free(t_bmp8 * img);
