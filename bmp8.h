26e5f2bb3463f6646d40778c092876054910dac7
laure <laure.szymanski@efrei.net>
2025-04-27 14:52:52 +0200

    free function added

--- bmp8.h
+++ bmp8.h
@@ -14,11 +14,6 @@ typedef struct {
     unsigned int colorDepth;
     unsigned int dataSize;
 } t_bmp8;
-t_bmp8 * bmp8_loadImage(const char * filename);
-void bmp8_saveImage(const char * filename, t_bmp8 * img);
-void bmp8_free(t_bmp8 * img);
-void bmp8_printInfo(t_bmp8 * img);
-#endif //BMP8_H
 
 //definition of the functions
 
