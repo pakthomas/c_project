95ec310af2d8def8777f35cd9e5912e995f87f1e
laure <laure.szymanski@efrei.net>
2025-04-27 14:53:17 +0200

    free function added

---
+++ cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/laure/CLionProjects/c_project/bmp8.c" "CMakeFiles/untitled3.dir/bmp8.c.obj" "gcc" "CMakeFiles/untitled3.dir/bmp8.c.obj.d"
+  "C:/Users/laure/CLionProjects/c_project/main.c" "CMakeFiles/untitled3.dir/main.c.obj" "gcc" "CMakeFiles/untitled3.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
