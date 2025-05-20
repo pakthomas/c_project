1e8b1acb65dda4fcc3075f54bc57de1bf3f77bdd
Thomas <pakthomas15@gmail.com>
2025-05-20 08:50:22 +0200

    added comments and the prototype of the functions for part 2

--- bmp8.c
+++ bmp8.c
@@ -127,9 +127,7 @@ void bmp8_threshold(t_bmp8 *img, int threshold) {
 void bmp8_applyFilter(t_bmp8 *img, float **kernel, int kernelSize) {
     int n = kernelSize / 2;
     unsigned char *newData = (unsigned char *)malloc(img->dataSize);
-
     memcpy(newData, img->data, img->dataSize);
-
     for (unsigned int y = 1;y < img->height-1; y++) {
         for (unsigned int x = 1; x < img->width -1;x++) {
             float sum = 0.0f;
@@ -147,129 +145,6 @@ void bmp8_applyFilter(t_bmp8 *img, float **kernel, int kernelSize) {
     free(img->data);
     img->data = newData;
 }
-t_bmp24 * bmp24_allocate (int width, int height, int colorDepth) {
-    t_bmp24 * img = (t_bmp24 *)malloc(sizeof(t_bmp24));
-    return img;
-}
-void file_rawRead (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
-fseek(file, position, SEEK_SET);
-fread(buffer, size, n, file);
-}
-
-void file_rawWrite (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
-    fseek(file, position, SEEK_SET);
-    fwrite(buffer, size, n, file);
-}
-
-void bmp24_readPixelData(t_bmp24 *image, FILE *file) {
-    // Get the offset to the pixel data (stored at byte 10 in the header)
-    uint32_t offset = *(uint32_t *)(((uint8_t *)&image->header) + BITMAP_OFFSET);
-    fseek(file, offset, SEEK_SET);
-
-    int width = image->width;
-    int height = image->height;
-
-    for (int y = height - 1; y >= 0; y--) { // BMP rows start from bottom to top and from left to right -_-
-        for (int x = 0; x < width; x++) {
-            uint8_t rgb[3];
-            if (fread(rgb, sizeof(uint8_t), 3, file) != 3) {
-                fprintf(stderr, "Error reading pixel at (%d, %d)\n", x, y);
-                return;
-            }
-            //store the RGB values of each pixel:
-            image->data[y][x].red   = rgb[0];
-            image->data[y][x].green = rgb[1];
-            image->data[y][x].blue  = rgb[2];
-        }
-    }
-}
-
-void bmp24_writePixelData (t_bmp24 * image, FILE * file) {
-    for (int y = image->height - 1; y >= 0; y--) {
-        for (int x = 0; x < image->width; x++) {
-            uint8_t rgb[3];
-            rgb[0] = image->data[y][x].red;
-            rgb[1] = image->data[y][x].green;
-            rgb[2] = image->data[y][x].blue;
-            fwrite(rgb, sizeof(uint8_t), 3, file); // stores the rgb composition of the pixel in the file
-                return;
-        }
-    }
-}
-
-
-
-
-t_bmp24 * bmp24_loadImage (const char * filename){
-    FILE * fptr;
-    t_bmp24 * img  = malloc(sizeof(t_bmp24));
-    fptr =fopen(filename,"rb");
-    if (fptr == NULL) {
-        printf("Error! The program will now exit.");
-        return 0;
-    }
-    t_bmp_header header;
-    t_bmp24 header_info;
-    file_rawRead(BITMAP_MAGIC, &header, sizeof(t_bmp_header), 1, fptr);
-    file_rawRead(BITMAP_SIZE, &header_info, sizeof(t_bmp_header), 1, fptr);
-    file_rawWrite(BITMAP_MAGIC, &header, sizeof(t_bmp_header), 1, img);
-    file_rawWrite(BITMAP_SIZE, &header_info, sizeof(t_bmp_header), 1, img);
-    bmp24_readPixelData(img, fptr);
-    fclose(fptr);
-    return img;
-}
-
-void bmp24_saveImage (t_bmp24 * img, const char * filename) {
-    FILE * fptr;
-    fptr =fopen(filename,"rb");
-    t_bmp_header header;
-    if (fptr == NULL) {
-        printf("Error! The program will now exit.");
-        return;
-    }
-    file_rawWrite( BITMAP_MAGIC,&header, HEADER_SIZE, 1, fptr);
-    bmp24_writePixelData(img,fptr);
-    fclose(fptr);
-}
-
-void bmp24_negative(t_bmp24 *img) {
-    for (unsigned int i = 0; i < img->height; i++) {
-        for (unsigned int j = 0; j < img->width; j++) {
-            img->data[i][j].red = 255 - img->data[i][j].red;
-            img->data[i][j].green = 255 - img->data[i][j].green;
-            img->data[i][j].blue = 255 - img->data[i][j].blue;
-        }
-    }
-}
-
-void bmp24_grayscale (t_bmp24 * img) {
-    for (unsigned int i = 0; i < img->height; i++) {
-        for (unsigned int j = 0; j < img->width; j++) {
-            int average = (img->data[i][j].red+img->data[i][j].green+img->data[i][j].blue)/3;
-            img->data[i][j].red = average;
-            img->data[i][j].green = average;
-            img->data[i][j].blue = average;
-        }
-    }
-}
-
-void bmp24_brightness (t_bmp24 * img,int value) {
-    for (unsigned int i = 0; i < img->height; i++) {
-        for (unsigned int j = 0; j < img->width; j++) {
-            if (img->data[i][j].red + value <= 255)
-            img->data[i][j].red = (img->data[i][j].red+value);
-            else img->data[i][j].red = 255;
-            if (img->data[i][j].green + value <= 255)
-            img->data[i][j].green = (img->data[i][j].green+value);
-            else img->data[i][j].green = 255;
-            if (img->data[i][j].blue + value <= 255)
-            img->data[i][j].blue = (img->data[i][j].blue+value);
-            else img->data[i][j].blue = 255;
-        }
-    }
-}
-
-
 
 
 
--- bmp8.h
+++ bmp8.h
@@ -8,24 +8,6 @@
 #include <stdint.h>
 
 //definition of a structured type t_bmp8
-// Offsets for the BMP header
-#define BITMAP_MAGIC 0x00 // offset 0
-#define BITMAP_SIZE 0x02 // offset 2
-#define BITMAP_OFFSET 0x0A // offset 10
-18
-24-bit Image and Color Image Processing
-#define BITMAP_WIDTH 0x12 // offset 18
-#define BITMAP_HEIGHT 0x16 // offset 22
-#define BITMAP_DEPTH 0x1C // offset 28
-#define BITMAP_SIZE_RAW 0x22 // offset 34
-// Magical number for BMP files
-#define BMP_TYPE 0x4D42 // 'BM' in hexadecimal
-// Header sizes
-#define HEADER_SIZE 0x0E // 14 octets
-#define INFO_SIZE 0x28 // 40 octets
-// Constant for the color depth
-#define DEFAULT_DEPTH 0x18 // 24
-
 typedef struct {
     unsigned char header[54];
     unsigned char colorTable[1024];
@@ -35,14 +17,6 @@ typedef struct {
     unsigned int colorDepth;
     unsigned int dataSize;
 } t_bmp8;
-typedef struct {
-    t_bmp_header header;
-    t_bmp_info header_info;
-    int width;
-    int height;
-    int colorDepth;
-    t_pixel **data;
-} t_bmp24;
 typedef struct {
     uint16_t type;
     uint32_t size;
@@ -68,14 +42,7 @@ typedef struct {
     uint8_t green;
     uint8_t blue;
 } t_pixel;
-typedef struct {
-    t_bmp_header header;
-    t_bmp_info header_info;
-    int width;
-    int height;
-    int colorDepth;
-    t_pixel **data;
-} t_bmp24;
+
 
 //definition of the functions
 
@@ -92,27 +59,5 @@ void bmp8_threshold(t_bmp8 *img, int threshold);
 //definition of the apllyfilter functions
 void bmp8_applyFilter(t_bmp8 *img, float **kernel, int kernelSize);
 
-//definition of allocation and deallocation functions
-t_pixel ** bmp24_allocateDataPixels (int width, int height);
-void bmp24_freeDataPixels (t_pixel ** pixels, int height);
-t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
-void bmp24_free (t_bmp24 * img);
-
-//Convulition filers
-t_pixel bmp24_convolution (t_bmp24 * img, int x, int y, float ** kernel, int kernelSize);
-
-//read and write pixel data
-void bmp24_readPixelValue (t_bmp24 * image, int x, int y, FILE * file);
-void bmp24_readPixelData (t_bmp24 * image, FILE * file);
-void bmp24_writePixelValue (t_bmp24 * image, int x, int y, FILE * file);
-void bmp24_writePixelData (t_bmp24 * image, FILE * file);
-
-
-//Loading and saving 24-bit Images
-t_bmp24 * bmp24_loadImage (const char * filename);
-void bmp24_saveImage (t_bmp24 * img, const char * filename);
-
-//Get access to header in 24 bytes image
-void file_rawRead (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file)
 
 #endif
