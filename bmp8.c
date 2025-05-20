b812bd1e6414b20dfb0b7de4e083374d48dbd8a1
laure <laure.szymanski@efrei.net>
2025-05-20 10:21:22 +0200

    compute 3.3

--- bmp8.c
+++ bmp8.c
@@ -268,6 +268,116 @@ void bmp24_brightness (t_bmp24 * img,int value) {
         }
     }
 }
+//creation of the compute histogram function, the function goes through each function and store its value in a list => the aim of this funcction is to count how many pixels there is for each gray color in the file
+void bmp8_computeHistogram(t_bmp8 *img, int histogram[256]) {
+    // initialise all possible value of the pixels to zero
+    for (int i = 0; i < 256; i++) {
+        histogram[i] = 0;
+    }
+
+    // go through every pixel and add +1 to the value of the color of the pixel
+    for (unsigned int i = 0; i < img->dataSize; i++) {
+        int gray = img->data[i];
+        histogram[gray]++;
+    }
+}
+void bmp8_equalizeHistogram(t_bmp8 *img) {
+    int histogram[256] = {0};
+
+    // step 1 : Calculate the histogram of the original image
+    for (unsigned int i = 0; i < img->dataSize; i++) {
+        histogram[img->data[i]]++;
+    }
+
+    // step 2 : Calculate the cumulative histogram using a CDF
+    int cdf[256];
+    cdf[0] = histogram[0];
+    for (int i = 1; i < 256; i++) {
+        cdf[i] = cdf[i - 1] + histogram[i];
+    }
+
+    // step 3 :Normalize the CDF to obtain a new scale of gray levels.
+    int totalPixels = img->width * img->height;
+    unsigned char mapping[256];
+    for (int i = 0; i < 256; i++) {
+        mapping[i] = (unsigned char)((cdf[i] - cdf[0]) * 255 / (totalPixels - cdf[0]));
+    }
+
+    // step 4 : Apply the transformation to each pixel of the original image using the new scale of gray levels.
+    for (unsigned int i = 0; i < img->dataSize; i++) {
+        img->data[i] = mapping[img->data[i]];
+    }
+}
+
+int * bmp8_computeHistogram(t_bmp8 *img) {
+    // Allocate memory for the histogram (256 gray levels, initialized to 0)
+    int *hist = (int *)calloc(256, sizeof(int));
+    if (hist == NULL) {
+        printf("Error: could not allocate memory for histogram\n");
+        return NULL;
+    }
+
+    // Loop over all pixels in the image data
+    for (int i = 0; i < img->dataSize; i++) {
+        // Get the pixel value (0-255)
+        int pixel = (unsigned char)img->data[i];
+        // Increase the count for that pixel value in the histogram
+        hist[pixel]++;
+    }
+
+    // Return the histogram array (int[256])
+    return hist;
+}
+
+
+
+int * bmp8_computeCDF(int *hist) {
+    // Allocate memory for the cumulative histogram (CDF)
+    int *cdf = (int *)calloc(256, sizeof(int));
+    if (cdf == NULL) {
+        printf("Error: could not allocate memory for CDF\n");
+        return NULL;
+    }
+
+    // Step 1: Compute the cumulative sum of the histogram
+    cdf[0] = hist[0]; // first value is same
+    for (int i = 1; i < 256; i++) {
+        cdf[i] = cdf[i - 1] + hist[i]; // cdf[i] = cdf[i-1] + hist[i]
+    }
+
+    // Step 2: Find the minimum non-zero value in the CDF (cdfmin)
+    int cdfmin = 0;
+    for (int i = 0; i < 256; i++) {
+        if (cdf[i] != 0) {
+            cdfmin = cdf[i];
+            break; // found it
+        }
+    }
+
+    // Step 3: Normalize the CDF using the given formula:
+    // hist_eq[i] = round((cdf[i] - cdfmin) / (N - cdfmin) * 255)
+    int N = cdf[255]; // total number of pixels in image
+
+    // Allocate memory for the equalized histogram (normalized CDF)
+    int *hist_eq = (int *)calloc(256, sizeof(int));
+    if (hist_eq == NULL) {
+        printf("Error: could not allocate memory for hist_eq\n");
+        free(cdf);
+        return NULL;
+    }
+
+    // Apply the normalization formula to each gray level
+    for (int i = 0; i < 256; i++) {
+        hist_eq[i] = (int)round(((double)(cdf[i] - cdfmin) / (N - cdfmin)) * 255);
+    }
+
+    // We don't need the raw CDF anymore, so free it
+    free(cdf);
+
+    // Return the normalized histogram (used for equalization)
+    return hist_eq;
+}
+
 
 
 
